cb2e7181201e8904c3c7fd0f3a884c045cb80dfe
Yarin <yarinbar@campus.technion.ac.il>
2018-12-24 23:51:45 +0200

    need to test on pi

--- stablize.py
+++ stablize.py
@@ -3,21 +3,37 @@
 from sense_hat import SenseHat
 from math import sin
 from math import cos
+from math import radians
+from math import degrees
 
 
 sense = SenseHat()
 
 # returns a dictionary with the corrected values
-def normalize_accel(x_accel, y_accel, z_accel, pitch, roll, yaw):
+def normalize_accel(hat):
     result = {}
     g = 1
-    result["x"] = x_accel + (g * cos(pitch))
-    result["y"] = y_accel + (g * sin(pitch))
-    print(result["x"])
+
+    # TODO: check whether raw is better than not raw
+    acceleration = hat.get_accelerometer_raw()
+    orientation = hat.get_orientation_radians()
+
+    x_accel_raw = acceleration["x"]
+    y_accel_raw = acceleration["y"]
+    z_accel_raw = acceleration["z"]
+
+    # X - roll, Y - pitch, Z - yaw
+    roll = orientation["roll"]
+    pitch = orientation["pitch"]
+    yaw = orientation["yaw"]
+
+    x_real = x_accel_raw - (x_accel_raw * cos(roll))
+    y_real = y_accel_raw - (y_accel_raw * sin(pitch))
+
 
 
 def main():
-    normalize_accel(1, 2, 3, 4, 5, 6)
+    normalize_accel(sense)
 
 if __name__== "__main__":
   main()
